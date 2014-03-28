757525b33245ece252ce7cfeafad0831e0bfc2d5
Camilo Sperberg <c.sperberg@bedandbreakfast.eu>
2014-03-28 18:09:33 +0100

    Adding basic (and untested) opcache restarts plugin

--- munin_opcache.php
+++ munin_opcache.php
@@ -10,6 +10,9 @@ if (function_exists('opcache_get_status'))
         'mem_used.value' => $data['memory_usage']['used_memory'],
         'mem_free.value' => $data['memory_usage']['free_memory'],
         'mem_wasted.value' => $data['memory_usage']['wasted_memory'],
+		'oom_restarts.value' => $data['opcache_statistics']['oom_restarts'],
+		'hash_restarts.value' => $data['opcache_statistics']['hash_restarts'],
+		'manual_restarts.value' => $data['opcache_statistics']['manual_restarts'],
     );
 }
 else
@@ -19,6 +22,9 @@ else
         'mem_used.value' => 0,
         'mem_free.value' => 0,
 		'mem_wasted.value' => 0,
+		'oom_restarts.value' => 0,
+		'hash_restarts.value' => 0,
+		'manual_restarts.value' => 0,
     );
 }
 
